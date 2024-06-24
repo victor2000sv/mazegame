69c16e14211ee8b61f3e89a1c89edaa53d14b473
Victor Svensson <victor.svensson@eavs.se>
2024-06-24 19:21:48 +0200

    58 minutes of work

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,177 @@
+"use client";
 import Image from "next/image";
+import { useState } from "react";
+
+type direction = "North" | "West" | "East" | "South";
 
 export default function Home() {
+  const API_URL = "https://mazegame.plingot.com";
+
+  const [possibleDirections, setPossibleDirections] = useState<direction[]>([]);
+  const [loading, setLoading] = useState(false);
+  const [gameToken, setGameToken] = useState("");
+  const [victorious, setVictorious] = useState(false);
+
+  const getPossibleDirections: any = async (token = gameToken) => {
+    const response = await fetch(`${API_URL}/Room/current`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: token,
+      },
+    });
+
+    if (response.ok) {
+      const { paths, effect } = await response.json();
+
+      const possibleDir = paths.map((item: any) => item.direction);
+      return {
+        newDirections: possibleDir,
+        victory: effect == "Victory",
+      };
+    }
+
+    return {
+      newDirections: null,
+      victory: false,
+    };
+  };
+
+  const move = async (direction: direction) => {
+    setLoading(true);
+    const response = await fetch(
+      `${API_URL}/Player/move?direction=${direction}`,
+      {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: gameToken,
+        },
+      }
+    );
+
+    if (response.ok) {
+      const { newDirections, victory } = await getPossibleDirections();
+      if (victory) {
+        setVictorious(true);
+        stopGame();
+      }
+
+      setPossibleDirections(newDirections);
+    }
+    setLoading(false);
+  };
+
+  const stopGame = async () => {
+    const response = await fetch(`${API_URL}/Game`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: gameToken,
+      },
+    });
+  };
+
+  const startGame = async () => {
+    //RANOMSIZE SEED
+    setLoading(true);
+
+    const response = await fetch(`${API_URL}/Game/start`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        type: "Graph",
+        size: 5,
+        seed: 0,
+      }),
+    });
+
+    if (response.ok) {
+      const { token } = await response.json();
+      setGameToken(token);
+
+      const { newDirections } = await getPossibleDirections(token);
+      setPossibleDirections(newDirections);
+    }
+
+    setLoading(false);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <>
+      <div className="fixed bottom-8 left-1/2 -translate-x-1/2">
+        {gameToken.trim().length > 0 && (
+          <div className="grid grid-cols-3 gap-0 w-64 h-64">
+            <button
+              className="col-start-2 col-span-1 bg-[#2c2c2c] shadow-lg text-white p-4 group"
+              disabled={!possibleDirections.includes("North") || loading}
+              onClick={() => move("North")}
+            >
+              <Image
+                src={"/icons/up.svg"}
+                alt=""
+                width={100}
+                height={100}
+                className="active group-disabled:inactive group-disabled:opacity-20"
+              />
+            </button>
+            <button
+              className="col-start-1 col-span-1 bg-[#2c2c2c] shadow-lg text-white p-4 group"
+              disabled={!possibleDirections.includes("West") || loading}
+              onClick={() => move("West")}
+            >
+              <Image
+                src={"/icons/back.svg"}
+                alt=""
+                width={100}
+                height={100}
+                className="active group-disabled:inactive group-disabled:opacity-20"
+              />
+            </button>
+            <button
+              className="col-start-3 col-span-1 bg-[#2c2c2c] shadow-lg text-white p-4 group"
+              disabled={!possibleDirections.includes("East") || loading}
+              onClick={() => move("East")}
+            >
+              <Image
+                src={"/icons/forward.svg"}
+                alt=""
+                width={100}
+                height={100}
+                className="active group-disabled:inactive group-disabled:opacity-20"
+              />
+            </button>
+            <button
+              className="col-start-2 col-span-1 bg-[#2c2c2c] shadow-lg text-white p-4 group"
+              disabled={!possibleDirections.includes("South") || loading}
+              onClick={() => move("South")}
+            >
+              <Image
+                src={"/icons/down.svg"}
+                alt=""
+                width={100}
+                height={100}
+                className="active group-disabled:inactive group-disabled:opacity-20"
+              />
+            </button>
+          </div>
+        )}
+        {gameToken.trim().length == 0 && (
+          <button
+            onClick={startGame}
+            className="p-4 uppercase text-white font-bold text-xl text-center bg-[#2c2c2c] disabled:opacity-20"
+            disabled={loading}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+            Start
+          </button>
+        )}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      {victorious && (
+        <h1 className=" text-6xl text-green-300 uppercase font-bold fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
+          Victory!!
+        </h1>
+      )}
+    </>
   );
 }
